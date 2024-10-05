687edab1fb1bef3388ea443fa2c46d3cb1f2192c
luthfisauqi17 <sauqiluthfi877@gmail.com>
2024-10-05 14:19:43 +0700

    hello_world_luthfisauqi17.go added

---
+++ scripts/hello_world_luthfisauqi17.go
@@ -0,0 +1,12 @@
+// LANGUAGE: Go
+// ENV: The Go Plaground
+// AUTHOR: luthfisauqi17
+// GITHUB: https://github.com/luthfisauqi17
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
